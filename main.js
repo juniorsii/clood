f5769cc6da3d07b7320a0c4ccd020c21f268ab1b
Junior Silva <juniorsilva@MacBook-Pro-de-Junior.local>
2025-01-23 09:19:50 -0300

    galery

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ const menuBtn = document.querySelector("#menu-btn");
 menuBtn.addEventListener("click", () => {
   mainHeader.dataset.state = mainHeader.dataset.state === "active" ? "closed" : "active";
 })
+
+
+// Seleciona os botões das tabs e os itens da galeria
+const tabButtons = document.querySelectorAll('.tab-btn');
+const imageItems = document.querySelectorAll('.image-item');
+
+// Adiciona o evento de clique a cada botão
+tabButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    // Remove a classe "active" de todos os botões
+    tabButtons.forEach((btn) => btn.classList.remove('underline','active'));
+    button.classList.add('underline', 'active');
+
+    // Filtra os itens da galeria com base na categoria
+    const category = button.dataset.category;
+    imageItems.forEach((item) => {
+      if (category === 'all' || item.classList.contains(category)) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  });
+});
